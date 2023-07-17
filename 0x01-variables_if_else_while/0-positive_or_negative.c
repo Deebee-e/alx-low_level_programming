7e90283059be2b7cba2b5232afd6e69f000597c6
Debbie <elideb4life@gmail.com>
2023-07-17 04:38:57 -0700

    if_else_statements

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,7 +5,7 @@
 /**
  * main - the block of the code
  * Description: print  random number using the source code
- * check if number is Positive, Negative or Zero.
+ * check if number is Positive, Negative or Zero
  * Return: 0
  */
 
@@ -16,11 +16,12 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	printf("The output is: %i\n", n);
-	
+
 	if (n < 0)
-	    printf("%i is negative\n", n);
+
+	printf("%i is negative\n", n);
 	else if (n > 0)
-	    printf("%i is positive\n", n);
+	printf("%i is positive\n", n);
 	else
 	printf("%i is zero\n", n);
 	return (0);
