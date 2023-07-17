40f6129b2c4d6db6488a6d7c6fc2945b0482ee51
Debbie <elideb4life@gmail.com>
2023-07-17 06:39:22 -0700

    if_else_statements

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -2,12 +2,12 @@
 #include <time.h>
 #include <stdio.h>
 
-/** 
+/**
  * main - the block of code
- * Description: a program that prints the last digit of the number stored in the variable n
+ * Description: a program that prints the last digit of the number
  * Return: 0
  */
- 
+
 int main(void)
 {
 	int n;
@@ -19,9 +19,9 @@ int main(void)
 	a = n % 10;
 
 	if (a > 5)
-	       	printf("Last digit of %i is %i and is greater than 5\n", n, a);
+		printf("Last digit of %i is %i and is greater than 5\n", n, a);
 	else if (n == 0)
-		 printf("Last digit of %i is %i and is 0\n",n, a);
+		 printf("Last digit of %i is %i and is 0\n", n, a);
 	else
 		printf("Last digit of %i is %i and is less than 6 and not 0\n", n, a);
 	return (0);
