651619e4ab7ccdad34a12de45ccc2516fc6e879c
Debbie <elideb4life@gmail.com>
2023-07-18 19:07:10 -0700

    error of compilation fixed

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * main- Write a function that prints the alphabet, in lowercase.
+ * print_alphabet- a function that prints the alphabet, in lowercase.
  *
  * Return: 0
  */
